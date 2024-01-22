5cba52442ebf0751aae3fadbea79fa0ced0001bd
Jean-Massumi <geanmassumit@hotmail.com>
2024-01-22 18:54:27 -0300

    Inicio do Primeiro trabalho

---
+++ trabalhos/trab1.py
@@ -0,0 +1,72 @@
+class Colecoes:
+    '''
+    Uma Coleção de figurinhas que permite gerenciar os álbuns e fazer trocas com os 
+    usuarios.
+    '''
+
+
+    def __init__(self) :
+        '''
+        Criação de uma coleção com a quantidade de figurinhas(número de figurinhas total do albúm)
+        '''
+
+        self.album = 60
+
+
+    def insercao(self, figurinha:int):
+        '''
+        Insere uma figurinha especifica no album do usuario
+        '''
+
+
+    def remocao(self,figurinha:int):
+        '''
+        Remove uma figurinha especifica no album do usuario.
+        '''
+
+
+    def gerar_str_figuras(self):
+        '''
+        Gera uma representação em string das figurinhas presentes em um álbum, 
+        sem considerar repetições.
+        '''
+
+
+    def gerar_str_figuras_repetidas(self):
+        '''
+        Gerar uma representação em string das figurinhas presentes em um álbum, 
+        indicando a quantidade de repetições.
+        
+        '''
+
+
+    def troca(self):
+        '''
+        Realizar a troca máxima de figurinhas entre duas coleções, garantindo que cada
+        coleção obtenha as figurinhas que não possui.
+        '''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
