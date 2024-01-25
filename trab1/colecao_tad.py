aa889c9178edfc4d0e00d5b6b145a4c07c989279
Jean-Massumi <geanmassumit@hotmail.com>
2024-01-25 16:54:17 -0300

    Especificacao do TAD

--- trab1/colecao_tad.py
+++ trab1/colecao_tad.py
@@ -1,8 +1,15 @@
+from __future__ import annotations
+from ed import array
+
 class Colecoes:
     '''
     Uma Coleção de figurinhas que permite gerenciar os álbuns e fazer trocas com os 
     usuarios.
     '''
+
+    capacidade_album = 60  
+    # A *capacidade_album* é uma limitacao que um album pode suportar
+    # uma certa quantidade x de figurinhas nela.
 
 
     def __init__(self) :
@@ -10,63 +17,116 @@ class Colecoes:
         Criação de uma coleção com a quantidade de figurinhas(número de figurinhas total do albúm)
         '''
 
-        self.album = 60
+        self.album = array(self.capacidade_album, 0)
 
 
     def insercao(self, figurinha:int):
         '''
         Insere uma figurinha especifica no album do usuario
+
+        Exemplos
+        >>> c = Colecao()
+        >>> c.insercao(58)
+        >>> c.gerar_str_figuras()
+        "[58]"
+        >>> c.insercao(12)
+        >>> c.gerar_str_figuras()
+        "[12, 58]"
         '''
 
 
-    def remocao(self,figurinha:int):
+    def remocao(self,figurinha:int) -> int:
         '''
         Remove uma figurinha especifica no album do usuario.
+
+        Requer que a figurinha esteja no album.
+
+        Exemplos
+        >>> c = Colecao()
+        >>> c.remocao(34)
+        Traceback (most recent call last):
+        ...
+        ValueError: Figurinha indiponivel
+        >>> c.insercao(28)
+        >>> c.remocao(28)
+        28
         '''
 
 
-    def gerar_str_figuras(self):
+    def gerar_str_figuras(self) -> str:
         '''
         Gera uma representação em string das figurinhas presentes em um álbum, 
         sem considerar repetições.
+
+        Exemplos
+        >>> c = Colecao()
+        >>> c.insercao(58)
+        >>> c.insercao(9)
+        >>> c.insercao(32)
+        >>> c.gerar_str_figuras()
+        "[9, 32, 58]"
+        >>> c.remocao(32)
+        32
+        >>> c.gerar_str_figuras()
+        "[9, 58]"
         '''
 
 
-    def gerar_str_figuras_repetidas(self):
+    def gerar_str_figuras_repetidas(self) -> str:
         '''
         Gerar uma representação em string das figurinhas presentes em um álbum, 
         indicando a quantidade de repetições.
-        
+
+        Exemplos
+        >>> c = Colecao()
+        >>> c.insercao(58)
+        >>> c.insercao(58)
+        >>> c.insercao(58)
+        >>> c.insercao(9)
+        >>> c.insercao(32)
+        >>> c.insercao(32)
+        >>> c.gerar_str_figuras_repetidas()
+        "[9 (1), 32 (2), 58 (3)]"
+        >>> c.remocao(32)
+        32
+        >>> c.gerar_str_figuras_repetidas()
+        "[9 (1), 32 (1), 58 (3)]"
         '''
 
 
-    def troca(self):
+    def troca(self, p1:int, p2:int):
         '''
         Realizar a troca máxima de figurinhas entre duas coleções, garantindo que cada
         coleção obtenha as figurinhas que não possui.
+        
+        Exemplos
+        # Usuario 1
+        >>> c1 = Colecao()
+        >>> c1.insercao(58)
+        >>> c1.insercao(58)
+        >>> c1.insercao(58)
+        >>> c1.insercao(9)
+        >>> c1.insercao(32)
+        >>> c1.insercao(32)
+        >>> c1.gerar_str_figuras_repetidas()
+        "[9 (1), 32 (2), 58 (3)]"
+        # Usuario 2
+        >>> c2 = Colecao()
+        >>> c2.insercao(60)
+        >>> c2.insercao(60)
+        >>> c2.insercao(60)
+        >>> c2.insercao(60)
+        >>> c2.insercao(4)
+        >>> c2.insercao(4)
+        >>> c2.gerar_str_figuras_repetidas()
+        "[4 (2), 60 (4)]"
+        # Realização da Troca
+        >>> c1.troca(58, 60)
+        >>> c1.gerar_str_figuras_repetidas()
+        "[9 (1), 32 (2), 60 (3)]"
+        >>> c2.gerar_str_figuras_repetidas()
+        "[4 (2), 58 (3), 60 (1)]"
         '''
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
